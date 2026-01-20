41abfd37fcee1e2f3c6d673f61015cd5266f39b4
Agata <agatadicalogero@gmail.com>
2026-01-20 11:55:57 +0100

    feat: customize README and theme for ZampaStore

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -3,6 +3,22 @@ import { provideRouter } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
+  beforeAll(() => {
+    if (!globalThis.matchMedia) {
+      const noop = () => undefined;
+      globalThis.matchMedia = (query: string): MediaQueryList => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: noop,
+        removeListener: noop,
+        addEventListener: noop,
+        removeEventListener: noop,
+        dispatchEvent: () => false,
+      });
+    }
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
